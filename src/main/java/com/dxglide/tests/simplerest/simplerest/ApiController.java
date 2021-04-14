6332bd781b05e07557bf7edc46e6a8a493a29974
dxglide <dxglide@gmail.com>
2021-04-14 22:04:04 +0300

    init

---
+++ src/main/java/com/dxglide/tests/simplerest/simplerest/ApiController.java
@@ -0,0 +1,21 @@
+package com.dxglide.tests.simplerest.simplerest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/")
+public class ApiController {
+
+//    @GetMapping(path="/", produces = "application/json")
+//    public String getdefault () {
+//        return "{ \"answer\" : DEFAULT }";
+//    }
+
+    @GetMapping(path="/hello", produces = "application/json")
+    public String  getHello() {
+        return "{ \"answer\" : Hello }";
+    }
+
+}
