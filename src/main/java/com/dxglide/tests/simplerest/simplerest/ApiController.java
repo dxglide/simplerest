ad24d681a5defe7382282dd258393f06e2277885
dxglide <dxglide@gmail.com>
2021-04-15 19:25:01 +0300

    ngix exmple. proxy

--- src/main/java/com/dxglide/tests/simplerest/simplerest/ApiController.java
+++ src/main/java/com/dxglide/tests/simplerest/simplerest/ApiController.java
@@ -7,10 +7,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 //@RequestMapping(path = "/")
 public class ApiController {
-
     @GetMapping(path="/", produces = "application/json")
     public String  getHello() {
         return "{ \"answer\" : \"Hello\" }";
     }
+
+
+    @GetMapping(path="/mano_testas_kitu_linku_tas_pas", produces = "application/json")
+    public String  getHello2() {
+        return "{ \"answer\" : \"Hello 2 kitu linku\" }";
+    }
+
 
 }
